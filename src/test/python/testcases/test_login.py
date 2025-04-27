438e8cc69cd063945dfa014b230794c8e459dc75
Manisha <manisha.sapra.eng@gmail.com>
2025-04-27 21:21:14 +0200

    27th April 2025

--- src/test/python/testcases/test_login.py
+++ src/test/python/testcases/test_login.py
@@ -15,34 +15,18 @@ def driver():
     driver.quit()
 
 
-def test_valid_login(driver):
-    data = test_data["valid_login"]
+@pytest.mark.parametrize("test_case", ["valid_login", "empty_username", "empty_password"])
+def test_login(driver, test_case):
+    data = test_data[test_case]
     login_page = LoginPage(driver)
     login_page.open()
     login_page.enter_username(data["username"])
     login_page.enter_password(data["password"])
     login_page.submit_login()
-    assert login_page.is_login_successful(), "Login should be successful with valid credentials."
 
-
-def test_empty_username(driver):
-    data = test_data["empty_username"]
-    login_page = LoginPage(driver)
-    login_page.open()
-    login_page.enter_username(data["username"])
-    login_page.enter_password(data["password"])
-    login_page.submit_login()
-    error_message = login_page.get_error_message()
-    assert error_message == "Please enter a username and password.", f"Unexpected error message for empty username: {error_message}"
-
-
-def test_empty_password(driver):
-    data = test_data["empty_password"]
-    login_page = LoginPage(driver)
-    login_page.open()
-    login_page.enter_username(data["username"])
-    login_page.enter_password(data["password"])
-    login_page.submit_login()
-    error_message = login_page.get_error_message()
-    assert error_message == "Please enter a username and password.", f"Unexpected error message for empty password: {error_message}"
-
+    if test_case == "valid_login":
+        assert login_page.is_login_successful(), "Login should be successful with valid credentials."
+    else:
+        error_message = login_page.get_error_message()
+        expected_error = "Please enter a username and password."
+        assert error_message == expected_error, f"Unexpected error message for {test_case}: {error_message}"
